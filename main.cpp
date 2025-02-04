ed3a2299bef69a554558733875921698f3c671fc
JordanTerwilliger <jordanterwilliger@hotmail.com>
2025-02-04 14:58:00 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int NUM_STUDENTS = 3;
+    string names[] = {"Sam", "Bob", "Joe"};
+    double gpas[] = {2.5, 3.4, 4.0};
+    int ids[] = {800123456, 801234546, 801224556};
+
+    for (int i = 0; i < NUM_STUDENTS; i++)
+    {
+        /* code */
+    }
+    
+
+    return 0;
+}
